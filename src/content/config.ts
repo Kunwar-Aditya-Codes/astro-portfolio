3f887eddb85bf41a723fe57b481e3daab511aeaa
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-02-04 10:46:33 +0530

    init: markdown

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const webProjectCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    stack: z.array(z.string()),
+    url: z.string(),
+    hero: z.string(),
+  }),
+});
+
+export const collections = {
+  webProjects: webProjectCollection,
+};
